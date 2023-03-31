c868db1fe8b757e8e8fed67bd44228ca40a972cb
Collin Wheat <collinwheat@gmail.com>
2023-03-31 18:43:31 -0400

    Create Boot REST Project

---
+++ PhotoPortfolio/src/test/java/com/skilldistillery/photoportfolio/PhotoPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.photoportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
